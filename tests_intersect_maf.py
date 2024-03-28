437128d3ae37f9bdccb67bbd3b2a4a97490d7cc0
k-hench <khench@posteo.net>
2024-03-28 14:27:52 +0100

    bugfix: use actual start position of seq

--- tests_intersect_maf.py
+++ tests_intersect_maf.py
@@ -4,6 +4,15 @@
 # gzipped input
 #  ./intersect_maf_bed -m tests/maf/test3.maf.gz -b tests/bed/A.bed.gz -r A -o /dev/stdout
 
+# no hits case
+# ./intersect_maf_bed -m tests/maf/test3.maf -b tests/bed/A_reverse.bed -r A -o /dev/stdout
+
+# negative strand case
+# ./intersect_maf_bed -m tests/maf/test4.maf -b tests/bed/A_reverse.bed -r A -o /dev/stdout
+
+# bad maf reference
+#./intersect_maf_bed -m tests/maf/test3_bad.maf -b tests/bed/multiple_hits.bed -r A -o /dev/stdout
+
 # merge bed regions
 #./intersect_maf_bed -m tests/maf/test3.maf -b tests/bed/overlaps.bed -r A -o /dev/stdout
 
