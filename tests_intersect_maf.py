4a472013b948895ab3d51d813b28fcf1b9c5cd96
k-hench <khench@posteo.net>
2024-03-28 12:58:05 +0100

    accept gz input

--- tests_intersect_maf.py
+++ tests_intersect_maf.py
@@ -1,10 +1,17 @@
 # basic use case
 #  ./intersect_maf_bed -m tests/maf/test3.maf -b tests/bed/A.bed -r A -o /dev/stdout
 
+# gzipped input
+#  ./intersect_maf_bed -m tests/maf/test3.maf.gz -b tests/bed/A.bed.gz -r A -o /dev/stdout
+
 # merge bed regions
 #./intersect_maf_bed -m tests/maf/test3.maf -b tests/bed/overlaps.bed -r A -o /dev/stdout
 
+# spanning several maf sequences
+#./intersect_maf_bed -m tests/maf/test3.maf -b tests/bed/spanning_multiple_maf_seqs.bed -r A -o /dev/stdout
+
 # extract mutliple regions from single maf block
+# ./intersect_maf_bed -m tests/maf/test3.maf -b tests/bed/multiple_hits.bed -r A -o /dev/stdout
 
 # wrong ref
 # ./intersect_maf_bed -m tests/maf/test3.maf -b tests/bed/A.bed -r B -o /dev/stdout
