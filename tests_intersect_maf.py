883707c4fc1a931a2628b12a87f2f188ab569938
k-hench <khench@posteo.net>
2024-03-28 12:47:14 +0100

    maf interct: enable mutliple hits

---
+++ tests_intersect_maf.py
@@ -0,0 +1,11 @@
+# basic use case
+#  ./intersect_maf_bed -m tests/maf/test3.maf -b tests/bed/A.bed -r A -o /dev/stdout
+
+# merge bed regions
+#./intersect_maf_bed -m tests/maf/test3.maf -b tests/bed/overlaps.bed -r A -o /dev/stdout
+
+# extract mutliple regions from single maf block
+
+# wrong ref
+# ./intersect_maf_bed -m tests/maf/test3.maf -b tests/bed/A.bed -r B -o /dev/stdout
+
